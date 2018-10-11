9bf6d212b8da0f8015a68a50bd14fc794b063ff1
Gregzenegair <gregzenegair@gmail.com>
2018-10-12 00:42:50 +0200

    UPD progress on miner

--- role.miner.js
+++ role.miner.js
@@ -33,126 +33,22 @@ var roleMiner = {
         if (null == creep.memory.containerSpot) {
             creep.memory.containerSpot = this.getFreeSpot(creep);
         }
-        
+
         helperCreep.moveTo(creep, creep.memory.containerSpot);
+        if (creep.pos.x === creep.memory.containerSpot.pos.x
+                && creep.pos.y === creep.memory.containerSpot.pos.y) {
+            creep.memory.mineSpotReached = true;
+        }
 
     },
 
     mineSpot: function (creep) {
-
-    },
-
-
-
-
-
-//delete after this
-    /** @param {Creep} creep **/
-    run: function (creep) {
-
-        if (null == Memory.miners) {
-            Memory.miners = [];
-        }
-
-        if (!creep.memory.canWork) {
-            var energySource = this.setEnergySource(creep);
-
-            if (energySource) {
-                switch (creep.memory.energySourceType) {
-                    case helperEnergy.ENERGY_SOURCE_TYPES.DROPPED:
-                        var creepPickupAction = creep.pickup(energySource);
-                        if (creepPickupAction === ERR_NOT_IN_RANGE) {
-                            this.moveTo(creep, energySource);
-                        }
-
-                        if (creepPickupAction === ERR_INVALID_TARGET) {
-                            creep.memory.energySourceId = null;
-                        }
-                        break;
-
-                    case helperEnergy.ENERGY_SOURCE_TYPES.DEPOSIT:
-                        var creepWithdrawAction = creep.withdraw(energySource, RESOURCE_ENERGY);
-                        if (creepWithdrawAction === ERR_NOT_IN_RANGE) {
-                            this.moveTo(creep, energySource);
-                        }
-
-                        if (creepWithdrawAction === ERR_INVALID_ARGS || creepWithdrawAction === ERR_INVALID_TARGET || creepWithdrawAction === ERR_NOT_ENOUGH_RESOURCES) {
-                            creep.memory.energySourceId = null;
-                        }
-                        break;
-
-                    default :
-                    case helperEnergy.ENERGY_SOURCE_TYPES.SOURCE:
-                        // console.log("energySource=" + energySource + " creep name=" + creep.name);
-                        creep.memory.filler = true;
-                        var creepHarvestAction = creep.harvest(energySource);
-                        if (creepHarvestAction === ERR_NOT_IN_RANGE) {
-                            this.moveTo(creep, energySource);
-                        }
-
-                        if (creepHarvestAction === ERR_NOT_ENOUGH_RESOURCES) {
-                            creep.memory.energySourceId = null;
-                        }
-                        break;
-                }
-
-            } else {
-                console.log("energySource is null or not found");
-                creep.memory.energySourceId = null;
-                creep.memory.energySourceType = null;
-                // TODO: set something to do if no energy source
-            }
-        }
-
-        if (creep.memory.canWork) {
-
-            var target = null;
-
-            if (creep.memory.filler) {
-                target = roleFiller.run(creep);
-            }
-        }
-    },
-
-    moveTo: function (creep, energySource) {
-        var moveResult = creep.moveTo(energySource, {
-            reusePath: 32,
-            visualizePathStyle: {stroke: '#fffd00'}
-        });
-
-        if (moveResult === ERR_NO_PATH) {
-            creep.memory.errorPathCount++;
-            if (creep.memory.errorPathCount > 3) {
-                creep.memory.errorPathCount = 0;
-                creep.memory.alternativePath = true;
-            }
+        if (creep.memory.mineSpotReached) {
+            var source = creep.pos.findClosestByRange(FIND_SOURCES);
+            creep.harvest(source);
         } else {
-            creep.memory.alternativePath = false;
+            this.moveToFreeSpot(creep);
         }
-
-        if (moveResult === ERR_NO_PATH && creep.memory.alternativePath) {
-
-            energySource = this.setEnergySource(creep, true);
-            creep.moveTo(energySource,
-                    {reusePath: 32, visualizePathStyle: {stroke: '#fffd00'}});
-        }
-    },
-
-    setEnergySource: function (creep) {
-        var energySource = null;
-        if (null != creep.memory.energySourceId) {
-            energySource = Game.getObjectById(creep.memory.energySourceId);
-        } else {
-            energySource = helperEnergy.findValidPathHarvestSource(creep);
-
-            if (energySource) {
-                Memory.miners[energySource.target.id] = creep.id;
-                creep.memory.energySourceId = energySource.target.id;
-                creep.memory.energySourceType = energySource.energySourceType;
-                energySource = energySource.target;
-            }
-        }
-        return energySource;
     }
 
 
