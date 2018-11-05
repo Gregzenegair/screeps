a5af853741d470643ed1e5b68cd0887f07793b2e
Gregzenegair <gregzenegair@gmail.com>
2018-11-05 22:30:30 +0100

    FIX undefined

--- role.miner.js
+++ role.miner.js
@@ -63,7 +63,8 @@ var roleMiner = {
         if (null != creep.memory.containerSpot) {
             var containerSpot = Game.getObjectById(creep.memory.containerSpot);
             helperCreep.moveTo(creep, containerSpot, true);
-            if (creep.pos.x === containerSpot.pos.x
+            if (null != creep && null != containerSpot
+                    && creep.pos.x === containerSpot.pos.x
                     && creep.pos.y === containerSpot.pos.y) {
                 creep.memory.mineSpotReached = true;
             }
