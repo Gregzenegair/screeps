2589e5cc663c295aec3c28b4fb7e9ae3ea4e9f01
Gregzenegair <gregzenegair@T800>
2019-10-07 20:48:37 +0200

    UPD level 8 rooms  now only have on miner and one utility

--- roles.setup.js
+++ roles.setup.js
@@ -6,20 +6,20 @@ require('object.extension')();
 
 var rolesSetup = {
 
-    UTILITY: {name: "utility", maxCount: -1, baseBody: [WORK, CARRY, MOVE], filler: true},
-    FILLER: {name: "filler", maxCount: -1, baseBody: [CARRY, MOVE], filler: true},
-    MINER: {name: "miner", maxCount: -1, baseBody: [MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, WORK], simpleBody: true},
-    CLAIMER: {name: "claimer", maxCount: 1, baseBody: [CLAIM, MOVE], simpleBody: false},
-    COMBAT: {name: "combat", maxCount: 1, baseBody: [TOUGH, TOUGH, ATTACK, MOVE, MOVE]},
-    COMBAT2: {name: "combat", maxCount: 1, baseBody: [TOUGH, ATTACK, MOVE]},
-    HEALER: {name: "heal", maxCount: 1, baseBody: [TOUGH, HEAL, MOVE, MOVE]},
+    UTILITY: { name: "utility", maxCount: -1, baseBody: [WORK, CARRY, MOVE], filler: true },
+    FILLER: { name: "filler", maxCount: -1, baseBody: [CARRY, MOVE], filler: true },
+    MINER: { name: "miner", maxCount: -1, baseBody: [MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, WORK], simpleBody: true },
+    CLAIMER: { name: "claimer", maxCount: 1, baseBody: [CLAIM, MOVE], simpleBody: false },
+    COMBAT: { name: "combat", maxCount: 1, baseBody: [TOUGH, TOUGH, ATTACK, MOVE, MOVE] },
+    COMBAT2: { name: "combat", maxCount: 1, baseBody: [TOUGH, ATTACK, MOVE] },
+    HEALER: { name: "heal", maxCount: 1, baseBody: [TOUGH, HEAL, MOVE, MOVE] },
 
     /** @param {Creep} creep **/
     spawn: function (type) {
         if (Game.time % 64 === 0 || null == Memory.utilityMaxCount) {
             if (null == Memory.utilityMaxCount || null == Memory.minerMaxCount || null == Memory.fillerUnitCount
-                    || Game.time % 512 === 0
-                    || null == Memory.previousUtilityMaxCount) {
+                || Game.time % 512 === 0
+                || null == Memory.previousUtilityMaxCount) {
 
                 if (null != Memory.utilityMaxCount) {
                     Memory.previousUtilityMaxCount = JSON.parse(JSON.stringify(Memory.utilityMaxCount));
@@ -34,7 +34,7 @@ var rolesSetup = {
                 Memory.minerMaxCount = {};
                 Memory.minerUnitCount = {};
 
-//                Memory.fillerMaxCount = {};
+                //                Memory.fillerMaxCount = {};
                 Memory.fillerUnitCount = {};
             }
 
@@ -53,14 +53,14 @@ var rolesSetup = {
             for (var key in Game.spawns) {
                 var spawn = Game.spawns[key];
 
-//                var assignableRooms = {};
-//
-//                assignableRooms = Game.map.describeExits(spawn.room.name);
-//                assignableRooms[spawn.room.name] = spawn.room.name; //add it self
-//
-//                for (var roomKey in assignableRooms) {
+                //                var assignableRooms = {};
+                //
+                //                assignableRooms = Game.map.describeExits(spawn.room.name);
+                //                assignableRooms[spawn.room.name] = spawn.room.name; //add it self
+                //
+                //                for (var roomKey in assignableRooms) {
 
-//                    var roomName = assignableRooms[roomKey];
+                //                    var roomName = assignableRooms[roomKey];
 
                 for (var roomName in Game.rooms) {
 
@@ -99,7 +99,7 @@ var rolesSetup = {
                     }
 
                     var seekTypes = _.filter(Game.creeps, (creep) => creep.memory.role == type.name && creep.memory.roomAssigned == room.name
-                                && creep.ticksToLive > 48); // replace a dying creep sooner (by not counting it if under 32ttl)
+                        && creep.ticksToLive > 48); // replace a dying creep sooner (by not counting it if under 32ttl)
 
                     if (type.name === "utility") {
                         Memory.utilityUnitCount[room.name] = seekTypes.length;
@@ -113,23 +113,23 @@ var rolesSetup = {
                         Memory.minerUnitCount[room.name] = seekTypes.length;
                     }
 
-//                    var spawn = helperRoom.findSpawn(room);
-//
-//                    if (null == spawn) {
-//                        console.log("Not spawning for room {" + roomName + "], no spawn found");
-//                        continue;
-//                    }
+                    //                    var spawn = helperRoom.findSpawn(room);
+                    //
+                    //                    if (null == spawn) {
+                    //                        console.log("Not spawning for room {" + roomName + "], no spawn found");
+                    //                        continue;
+                    //                    }
 
                     if (!spawnForItself &&
-                            (Memory.utilityUnitCount[spawn.room.name] < Memory.utilityMaxCount[spawn.room.name]
-                                    || Memory.minerUnitCount[spawn.room.name] < Memory.minerMaxCount[spawn.room.name])) {
+                        (Memory.utilityUnitCount[spawn.room.name] < Memory.utilityMaxCount[spawn.room.name]
+                            || Memory.minerUnitCount[spawn.room.name] < Memory.minerMaxCount[spawn.room.name])) {
                         console.log("Not spawning spawn room [" + spawn.room.name + "] for this room " + room.name + " yet, current spawn has not yet it's max utility units");
                         continue;
                     }
 
                     if (!(type.name === this.MINER.name || type.name === this.UTILITY.name || type.name === this.FILLER.name)) {
                         var foundTowers = room.findInMemory(FIND_MY_STRUCTURES, {
-                            filter: {structureType: STRUCTURE_TOWER}
+                            filter: { structureType: STRUCTURE_TOWER }
                         });
 
                         var emptyTower = false;
@@ -145,7 +145,7 @@ var rolesSetup = {
                         }
                     }
 
-//                    console.log("Current status for spawn=" + spawn.name + ", " + type.name + "=" + seekTypes.length);
+                    //                    console.log("Current status for spawn=" + spawn.name + ", " + type.name + "=" + seekTypes.length);
 
                     if ((type.name === this.CLAIMER.name || type.name === this.COMBAT.name || type.name === this.HEALER.name) && Game.cpu.bucket < 5000) {
                         console.log("Not building combat, bucket is under 5000");
@@ -165,54 +165,54 @@ var rolesSetup = {
                     }
 
                     if (type.name === this.FILLER.name
-                            && (Memory.minerUnitCount[room.name] < Memory.minerMaxCount[room.name]
-                                    || Memory.minerMaxCount[room.name] === 0)) {
+                        && (Memory.minerUnitCount[room.name] < Memory.minerMaxCount[room.name]
+                            || Memory.minerMaxCount[room.name] === 0)) {
                         console.log("Not spawning fillers for this room yet, miners still not exists");
                         continue;
                     }
 
                     if (!spawnForItself &&
-                            (type.name === this.UTILITY.name
-                                    || type.name === this.MINER.name)
-                            && room.findInMemory(FIND_HOSTILE_CREEPS).length > 0) {
+                        (type.name === this.UTILITY.name
+                            || type.name === this.MINER.name)
+                        && room.findInMemory(FIND_HOSTILE_CREEPS).length > 0) {
                         console.log("Not spawning utilities for this room yet, assigned room is dangerous");
                         continue;
                     }
 
                     if (seekTypes.length < type.maxCount) {
                         if ((type.name === this.COMBAT.name && Memory.utilityUnitCount[room.name] < Memory.utilityMaxCount[room.name])
-                                || (type.name === this.COMBAT.name && null != room.controller && room.controller.my && room.controller.level <= 3)
-                                && Memory.hasBeenUnderAttack < 6) {
+                            || (type.name === this.COMBAT.name && null != room.controller && room.controller.my && room.controller.level <= 3)
+                            && Memory.hasBeenUnderAttack < 6) {
                             console.log('Not spawning combat yet, reason: not enough utility or room.controller.level <= 3');
                             continue;
                         }
 
                         if ((type.name === this.HEALER.name && Memory.utilityUnitCount[room.name] < Memory.utilityMaxCount[room.name])
-                                || (type.name === this.HEALER.name && null != room.controller && room.controller.my && room.controller.level <= 3)
-                                && Memory.hasBeenUnderAttack < 9
-                                || (null != room.controller && !room.controller.my)
-                                || null == room.controller) {
+                            || (type.name === this.HEALER.name && null != room.controller && room.controller.my && room.controller.level <= 3)
+                            && Memory.hasBeenUnderAttack < 9
+                            || (null != room.controller && !room.controller.my)
+                            || null == room.controller) {
                             console.log('Not spawning heal yet, reason: not enough utility or room.controller.level <= 3');
                             continue;
                         }
 
                         if ((!spawnForItself && type.name === this.CLAIMER.name)
-                                || (type.name === this.CLAIMER.name && Memory.utilityUnitCount[room.name] < Memory.utilityMaxCount[room.name])
-                                || (type.name === this.CLAIMER.name && null != room.controller && room.controller.my && room.controller.level <= 5)
-                                || (null != room.controller && !room.controller.my)
-                                || null == room.controller) {
+                            || (type.name === this.CLAIMER.name && Memory.utilityUnitCount[room.name] < Memory.utilityMaxCount[room.name])
+                            || (type.name === this.CLAIMER.name && null != room.controller && room.controller.my && room.controller.level <= 5)
+                            || (null != room.controller && !room.controller.my)
+                            || null == room.controller) {
                             console.log('Not spawning claimer yet, reason: not enough utility or room.controller.level <= 3');
                             continue;
                         }
 
                         if ((type.name === this.UTILITY.name || type.name === this.MINER.name)
-                                && helperRoom.roomDataGetter(room.name, helperRoom.MEMORY_KEYS.DANGEROUS_ROOM && !spawnForItself)) {
+                            && helperRoom.roomDataGetter(room.name, helperRoom.MEMORY_KEYS.DANGEROUS_ROOM && !spawnForItself)) {
                             console.log('Not spawning utility or miner for room[' + room.name + '], reason: too dangerous');
                             continue;
                         }
 
                         if (null != Memory.roomSpawnedType[room.name] && null != Memory.roomSpawnedType[room.name][type.name]
-                                && Memory.roomSpawnedType[room.name][type.name]) {
+                            && Memory.roomSpawnedType[room.name][type.name]) {
                             console.log('Not spawning [' + type.name + '] for room[' + room.name + '], reason: just already spawned');
                             continue;
                         }
@@ -246,10 +246,19 @@ var rolesSetup = {
         if (null != room.controller) {
             roomControlerLevel = room.controller.level;
         }
-        if (null != room.controller && room.controller.my && roomControlerLevel > 3
-                && null != Memory.previousUtilityMaxCount[room.name]
-                && null != Memory.minerMaxCount[room.name]
-                && Memory.minerMaxCount[room.name] > 0) {
+        if (null != room.controller && room.controller.my && roomControlerLevel == 8
+            && null != Memory.previousUtilityMaxCount[room.name]
+            && null != Memory.minerMaxCount[room.name]
+            && Memory.minerMaxCount[room.name] > 0) {
+
+            Memory.previousUtilityMaxCount[room.name] = 1;
+
+            return 1;
+
+        } else if (null != room.controller && room.controller.my && roomControlerLevel > 3
+            && null != Memory.previousUtilityMaxCount[room.name]
+            && null != Memory.minerMaxCount[room.name]
+            && Memory.minerMaxCount[room.name] > 0) {
             var result = 0;
             var upgraded = false;
             var droppedResources = room.findInMemory(FIND_DROPPED_RESOURCES);
@@ -300,6 +309,13 @@ var rolesSetup = {
     },
 
     calcMaxMiner: function (room) {
+        if (null != room.controller && room.controller.my && roomControlerLevel == 8
+            && null != Memory.previousUtilityMaxCount[room.name]
+            && null != Memory.minerMaxCount[room.name]
+            && Memory.minerMaxCount[room.name] > 0) {
+            return 1;
+        }
+
         var result = 0;
         var sources = room.findInMemory(FIND_SOURCES);
         for (var i = 0; i < sources.length; i++) {
